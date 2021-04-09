3a2d757d307ef87122c2833f3f6584175dd4dce7
Atte <atte.lihtamo@student.lut.fi>
2021-04-09 15:33:24 +0300

    added UI files from local project

--- app/src/main/java/com/github/sropelinen/olioht/MainActivity.java
+++ app/src/main/java/com/github/sropelinen/olioht/MainActivity.java
@@ -1,14 +1,99 @@
 package com.github.sropelinen.olioht;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.ActionBar;
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.drawerlayout.widget.DrawerLayout;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
+import android.content.res.Configuration;
 import android.os.Bundle;
+import android.os.PersistableBundle;
+import android.view.MenuItem;
+import android.view.View;
+
+import com.google.android.material.navigation.NavigationView;
 
 public class MainActivity extends AppCompatActivity {
+    DrawerLayout drawerLayout;
+    Toolbar toolbar;
+    ActionBarDrawerToggle toggle;
+    NavigationView navigationView;
+    Fragment fragment = null;
+    Fragment homeFragment = new HomeFragment();
+    ActionBar actionBar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        toolbar = findViewById(R.id.toolbar_main);
+        drawerLayout = findViewById(R.id.drawer_layout_main);
+        navigationView = findViewById(R.id.navigationView);
+
+        //set home on launch
+        FragmentManager manager = getSupportFragmentManager();
+        FragmentTransaction transaction = manager.beginTransaction();
+        transaction.replace(R.id.frame_main, homeFragment);
+        transaction.commit();
+
+        actionBar = getSupportActionBar();
+        if (actionBar != null) {
+            toggle = new ActionBarDrawerToggle(this, drawerLayout, toolbar,
+                    R.string.open_drawer_text, R.string.close_drawer_text){};
+            drawerLayout.addDrawerListener(toggle);
+            actionBar.setDisplayHomeAsUpEnabled(true);
+            actionBar.setHomeButtonEnabled(true);
+            actionBar.setTitle("Home");
+            toggle.syncState();
+            setFragment();
+        }
+    }
+
+
+
+    private void setFragment() {
+        navigationView.setNavigationItemSelectedListener(item -> {
+            int id = item.getItemId();
+            if (id == R.id.menu_home) {
+                fragment = homeFragment;
+            } else if (id == R.id.menu_settings) {
+                // veikka
+            } else if (id == R.id.menu_charts) {
+                // veikka
+            }
+            FragmentManager manager = getSupportFragmentManager();
+            FragmentTransaction transaction = manager.beginTransaction();
+            transaction.replace(R.id.frame_main, fragment);
+            transaction.commit();
+            drawerLayout.closeDrawers();
+            actionBar.setTitle(item.getTitle());
+            return false;
+        });
+    }
+
+    @Override
+    public void onPostCreate(@Nullable Bundle savedInstanceState, @Nullable PersistableBundle persistentState) {
+        super.onPostCreate(savedInstanceState, persistentState);
+        toggle.syncState();
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if (toggle.onOptionsItemSelected(item)) {
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public void onConfigurationChanged(@NonNull Configuration newConfig) {
+        super.onConfigurationChanged(newConfig);
+        toggle.onConfigurationChanged(newConfig);
     }
 }
