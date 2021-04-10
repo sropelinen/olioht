1d1c7d73beaf76d7fdd9d550b69a9a6dbe944b1c
Atte <atte.lihtamo@student.lut.fi>
2021-04-10 16:29:46 +0300

    created ViewModel class

--- app/src/main/java/com/github/sropelinen/olioht/SettingsFragment.java
+++ app/src/main/java/com/github/sropelinen/olioht/SettingsFragment.java
@@ -2,14 +2,19 @@ package com.github.sropelinen.olioht;
 
 import android.os.Bundle;
 
+import androidx.appcompat.widget.SwitchCompat;
 import androidx.fragment.app.Fragment;
+import androidx.lifecycle.ViewModelProvider;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.CompoundButton;
 
 public class SettingsFragment extends Fragment {
-     private View view;
+    private SettingsViewModel viewModel;
+    private View view;
+    SwitchCompat switchDarkMode;
 
     public SettingsFragment() {
         // Required empty public constructor
@@ -19,6 +24,10 @@ public class SettingsFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         view = inflater.inflate(R.layout.fragment_settings, container, false);
+        viewModel = new ViewModelProvider(getActivity()).get(SettingsViewModel.class);
+
+        switchDarkMode = view.findViewById(R.id.switch_dark_mode);
+        switchDarkMode.setOnCheckedChangeListener((buttonView, isChecked) -> viewModel.setDarkMode(isChecked));
         return view;
     }
 
