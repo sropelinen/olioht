83b6d83fad6f4cbee0c8bb07e653ef489d7d0fd3
sropelinen <sjr@tuta.io>
2021-04-10 17:58:53 +0300

    added view log

--- app/src/main/java/com/github/sropelinen/olioht/SettingsFragment.java
+++ app/src/main/java/com/github/sropelinen/olioht/SettingsFragment.java
@@ -7,18 +7,24 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 public class SettingsFragment extends Fragment {
      private View view;
+     private TextView showLog;
+     private final Profile profile;
 
     public SettingsFragment(Profile profile) {
-        // Required empty public constructor
+        this.profile = profile;
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         view = inflater.inflate(R.layout.fragment_settings, container, false);
+
+        showLog = view.findViewById(R.id.showLog);
+        showLog.setOnClickListener(v -> toggleLog());
         return view;
     }
 
@@ -26,4 +32,14 @@ public class SettingsFragment extends Fragment {
     public void goProfile(View view) {
 
     }
+
+    private void toggleLog() {
+        // ToDo kunnol tää
+        if (showLog.getText().toString().equals("View log")) {
+            showLog.setText(profile.getLog());
+        } else {
+            showLog.setText("View log");
+        }
+    }
+
 }
